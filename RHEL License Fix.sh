5c7da8ce03a6f1cf21ce1165b059055b30da8fbc
Kitime <bhardy0630@gmail.com>
2016-12-12 23:40:56 -0500

    Revised
    Revised comments, and cleanup added on "Quit if no action".

--- RHEL License Fix.sh
+++ RHEL License Fix.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# --------------Description--------------------
-# 
-# RHEL licensing "systemID" to subscription-manager" fixer - written for SingleHop, LLC by bhardy.
-# Should be a "wget and run" type script for fixing RHEL licensing - since we're using the new licensing system 
-# then we might as well make it as automated as possible.
-# 
 # ---------------Begin Init--------------------
 # 
 # defining "print" type function called "pause":
@@ -12,7 +6,7 @@
 function pause(){
 	read -p -r "$*" # may need a semicolon after if not working.
 }
-# Logging function! Logs to file /root/rhellicensefix.txt
+# Logging function - Logs to file /root/rhellicensefix.txt
 function log(){
 	tee -a /root/rhellicensefix.txt; printf "\n";
 } 
@@ -23,7 +17,7 @@ function disableplugin(){
 	PLGST=$(< /etc/yum/pluginconf.d/rhnplugin.conf | grep enabled); 
 	echo "Status of plugin is $PLGST , should read as enabled = 0" | log;
 }
-# ---------------Begin Main AKA (spaghetti.sh)--------------------
+# ------------------------Begin----------------------------
 # 
 echo #
 echo -e "\e[1;101m <<<License fix for RHEL version 5/6>>> \e[0m"
@@ -40,32 +34,33 @@ echo #
 # If we do see a valid registration, then we can re-register the UUID and ensure the RHN plugin is disabled.
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	echo "Looks registered - Grab the UUID for this server($HOSTNAME) in the RHN portal. If there's duplicates, delete the non-matching UUID." | log
+	echo "Looks registered - Grab the UUID for this server (This should appear as $HOSTNAME ) in your Red Hat Network portal." | log
+	echo "If there exist duplicates, delete the non-matching UUID." | log
 	pause "$PS2 Press [ENTER] when ready to proceed...."
 # Assign the UUID to a variable "UUID"
 	read -p -r "$PS2 Enter the UUID to (re)register: " UUID
-	printf "User entered UUID $UUID \n" > /root/rhellicensefix.txt
+# This following line is *only logged*, not actually displayed while running the script.
+	echo -e "\e[1;101m Received UUID $UUID \e[0m" >> /root/rhellicensefix.txt
 # Now, cleaning, then registering the UUID.
 	echo "Removing old/corrupted license data..." | log
 	subscription-manager clean | log
 # Disabling the RHN plugin so the client doesn't get a confusing error message. 
 # I turned this into a function but keeping it here just in case.
-# --------------------------Oh God I Called It Twice So It Needs To Be A Function-----------------
+# ------------This is now "disableplugin" function-----------------
 #	echo "Disabling classic style RHN plugin..." | log
 #	sed -i 's/enabled = 1/enabled = 0/g' /etc/yum/pluginconf.d/rhnplugin.conf
 #	PLGST=$(< /etc/yum/pluginconf.d/rhnplugin.conf | grep enabled)
 #	echo "Status of plugin is $PLGST , should be enabled = 0" | log
-# --------------------------Next Line Is Function-------------------------------------------------
+# -------------End of pre-function code-----------------------------
 	disableplugin
 	echo -e "Registering using UUID:\e[1;4;32m$UUID...\e[0m"
 	subscription-manager register --consumerid="$UUID" | log
-# Cleaning Repo cache	
+# Cleaning Repo cache, updating subscriptions...
 	echo "Cleaning repo data..." | log
 	yum clean all >> /etc/yum/pluginconf.d/rhnplugin.conf
-# Usually not needed, but just in case.
 	echo "Updating subscriptions..." | log
 	subscription-manager subscribe --auto
-# One last verification that we are indeed subscribed.
+# One last verification that we are actually subscribed.
 	echo "Confirming we're registered properly now..."
 	subscription-manager identity | grep -v org  | log
 elif [[ $REPLY =~ ^[Nn]$ ]] # You pressed "n" for no.
@@ -75,26 +70,27 @@ then
 	rm -f /etc/sysconfig/rhn/systemid
 # Disabling plugin
 	disableplugin
-# Get them SS creds ready...
+# Get them RHEL license creds ready, then enter them to register with new entitlements.
 	echo "You'll need the RHN credentials to proceed further" | log
 	pause "$PS2 Press [ENTER] when you have them available." | log
-# Now enter them, cause we're not hard-coding them into this script.
 	subscription-manager register | log
-# Normal setup is easy - enter the credentials and it registers you.
 	echo "Now we should be registered. Confirming..." | log
-# And then confirming that it's good.
+# confirming that it's good.
 	subscription-manager identity | log
 	echo "Updating subscriptions..." | log
 	subscription-manager subscribe --auto
-# Seriously. Something has gone terribly wrong.
-	echo "If you're not registered now, then something has gone very, very wrong - Escalate."
-# Clearing Yum Cache, as we may have changed some information.
+# My idea of "error handling".
+	echo "If you're not registered now, then something has gone very, very wrong - contact support."
+# Clear repo cache, transation test.
 	echo "Final testing/cleanup..."
 	yum clean all | log
-# Basically, a transaction test. Cross your fingers.
-	echo "Determining whether there are any updates available (I.E. can we hit the repos successfully?)"
-	yum check-update
-	echo "Finished!"
+	echo "Determining whether we can hit the Red Hat repositories successfully..."
+	yum check-update | log
+	echo "Finished."
+	echo "Check the file /root/rhellicensefix.txt for additional data."
 else
-	echo "Exiting!"
+	echo "Quitting - Reverting files created during init..."
+	rm -f /root/systemid.bak
+	rm -f /root/rhellicensefix.txt
+	echo "Nothing done - exiting."
 	fi
